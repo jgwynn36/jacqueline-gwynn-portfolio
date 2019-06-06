b662ba7e79aabf460df977a8845781ed4fdf7257
Jacqueline Gwynn <jacquelinegwynn@jacquelines-air.attlocal.net>
2019-06-05 22:08:25 -0400

    Updates

---
+++ precache-manifest.9e5e1685144fe3f17cff6242e8ac7c88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f39db502f690c2f2ac1c5372da38d9b6",
+    "url": "/jacqueline-gwynn-portfolio/index.html"
+  },
+  {
+    "revision": "ef9c5747860711dacdf2",
+    "url": "/jacqueline-gwynn-portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/jacqueline-gwynn-portfolio/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ef9c5747860711dacdf2",
+    "url": "/jacqueline-gwynn-portfolio/static/js/main.8d31968b.chunk.js"
+  },
+  {
+    "revision": "6ed716f51b0960d61806",
+    "url": "/jacqueline-gwynn-portfolio/static/js/runtime~main.1b20c8fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jacqueline-gwynn-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
